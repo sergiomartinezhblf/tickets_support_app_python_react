8ea9dec1425883a25c6d69470826a8e40214b00d
Sergio Martinez <sergiomartinezhblf@htomail.com>
2026-06-16 23:44:45 -0600

    Added requirements.txt and update database.py

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/support_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:password@localhost:5432/support_db")
 
 engine =create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine)
 
 Base = declarative_base()
